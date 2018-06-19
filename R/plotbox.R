0e0927b96b01098cf32dfda7932efeb5c54f6b8c
osoramirez <oscar.ramirez.alan@una.ac.cr>
2018-06-19 14:50:49 -0600

    updates

--- R/plotbox.R
+++ R/plotbox.R
@@ -24,11 +24,15 @@ plotbox <- function(x) {
   Median <- round(median(x, na.rm = TRUE), 3)
   Max <- round(max(x, na.rm = TRUE), 3)
   Min <- round(min(x, na.rm = TRUE), 3)
+  Q1<-round(quantile(x, 0.25, na.rm = TRUE), 3)
+  Q3<-round(quantile(x, 0.75, na.rm = TRUE), 3)
   points(mean(x), pch=20, cex = 1.8, col="black")
   text((Mean), "Mean", col="#918686", font=8, cex = .7, adj=-5)
   text((Median), "Median", col="#918686", font=8, cex = .7, adj=-3.7)
   text((Min), "Min", col="#918686", font=8, cex = .7, adj=-3.7)
   text((Max), "Max", col="#918686", font=8, cex = .7, adj=-3.7)
+  text((Q1), "Q1", col="#918686", font=7, cex = .5, adj=-10)
+  text((Q3), "Q3", col="#918686", font=7, cex = .5, adj=-10)
   summary(x)
 }
 
